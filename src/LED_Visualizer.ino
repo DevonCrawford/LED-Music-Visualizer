48226cf3be8e23254bc8731560d947d823058302
Cormac <cfarr311y@gmail.com>
2023-11-19 23:51:42 +0000

    Update LED_Visualizer.ino

--- src/LED_Visualizer.ino
+++ src/LED_Visualizer.ino
@@ -1,161 +1,118 @@
-/**
- * LED Music Visualizer
- * by Devon Crawford
- * using the FastLED library: http://fastled.io/
- * April 22, 2018
- * Watch the video: https://youtu.be/lU1GVVU9gLU
- */
 #include "FastLED.h"
 
-#define NUM_LEDS 300        // How many leds in your strip?
-#define updateLEDS 8        // How many do you want to update every millisecond?
-#define COLOR_SHIFT 180000  // Time for colours to shift to a new spectrum (in ms)
-CRGB leds[NUM_LEDS];        // Define the array of leds
+// Define constants
+#define NUM_LEDS 300
+#define UPDATE_LEDS 8
+#define COLOR_SHIFT 180000
+#define MIN_BRIGHTNESS 0.2
+#define MAX_BRIGHTNESS 1.0
+#define BRIGHTNESS_DIVIDER 614.0
 
-// Define the digital I/O PINS..
-#define DATA_PIN 6          // led data transfer
-#define PITCH_PIN 0         // pitch input from frequency to voltage converter
-#define BRIGHT_PIN 4        // brightness input from amplified audio signal
+// Define LED array and color structure
+CRGB leds[NUM_LEDS] = {};
 
-// Don't touch these, internal color variation variables
-unsigned long setTime = COLOR_SHIFT;
+struct Color {
+  uint8_t r, g, b;
+};
+
+// Define variables
+unsigned long colorShiftInterval = COLOR_SHIFT;
 int shiftC = 0;
 int mulC = 2;
 
-// Define color structure for rgb
-struct color {
-  int r;
-  int g;
-  int b;
-};
-typedef struct color Color;
+// Setup function
+void setup() {
+  // Initialize serial communication
+  Serial.begin(9600);
+  // Add NEOPIXEL LEDs to FastLED library
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  // Set input pins for pitch and brightness
+  pinMode(PITCH_PIN, INPUT);
+  pinMode(BRIGHT_PIN, INPUT);
 
-void setup() { 
-    Serial.begin(9600);
-  	FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
-    pinMode(A0, INPUT);
-    pinMode(A4, INPUT);
-
-    for(int i = 0; i < NUM_LEDS ; i++) {
-      leds[i] = CRGB(0,0,0);
-    }
-    FastLED.show();
-}
-
-void loop() { 
-  unsigned long time = millis();
-
-  // Shift the color spectrum by 200 on set intervals (setTime)
-  if(time / (double)setTime >= 1) {
-    setTime = time + COLOR_SHIFT;
-    Serial.println(setTime);
-    shiftC += 200;
-    mulC++;
-    if(shiftC >= 600) {
-      shiftC = 0;
-    }
-    if(mulC > 3) {
-      mulC = 2;
-    }
-  }
-
-  // Shift all LEDs to the right by updateLEDS number each time
-  for(int i = NUM_LEDS - 1; i >= updateLEDS; i--) {
-    leds[i] = leds[i - updateLEDS];
-  }
-
-  // Get the pitch and brightness to compute the new color
-  int newPitch = (analogRead(PITCH_PIN)*2) + shiftC;
-  Color nc = pitchConv(newPitch, analogRead(BRIGHT_PIN));
-
-  // Set the left most updateLEDs with the new color
-  for(int i = 0; i < updateLEDS; i++) {
-    leds[i] = CRGB(nc.r, nc.g, nc.b);
-  }
+  // Fill LED array with black color
+  fillLedsWithBlack();
+  // Display initial state
   FastLED.show();
-
-  //printColor(nc);
-  delay(1);
 }
 
-/**
- * Converts the analog brightness reading into a percentage
- * 100% brightness is 614.. about 3 volts based on frequency to voltage converter circuit
- * The resulting percentage can simply be multiplied on the rgb values when setting our colors,
- * for example black is (0,0,0) so when volume is off we get 0v and all colors are black (leds are off)
- */
+// Main loop function
+void loop() {
+  // Shift color spectrum at regular intervals
+  if (millis() >= colorShiftInterval) {
+    colorShiftInterval += COLOR_SHIFT;
+    shiftColorSpectrum();
+  }
+
+  // Update LEDs and set color based on pitch and brightness
+  updateLeds();
+  setUpdatedLedsColor(pitchConv((analogRead(PITCH_PIN) * 2) + shiftC, analogRead(BRIGHT_PIN)));
+  // Display the updated LEDs
+  FastLED.show();
+}
+
+// Convert brightness to a normalized value
 double convBrightness(int b) {
-  double c = b / 614.0000;
-  if( c < 0.2 ) {
-    c = 0;
-  }
-  else if(c > 1) {
-    c = 1.00;
-  }
-  return c;
+  double c = b / BRIGHTNESS_DIVIDER;
+  // Ensure brightness is within valid range
+  return (c < MIN_BRIGHTNESS) ? 0 : (c > MAX_BRIGHTNESS) ? 1.0 : c;
 }
 
-/**
- * Creates a new color from pitch and brightness readings
- * int p         analogRead(pitch) representing the voltage between 0 and 5 volts
- * double b      analogRead(brightness) representing volume of music for LED brightness
- * returns Color structure with rgb values, which appear synced to the music
- */
+// Convert pitch and brightness to RGB color
 Color pitchConv(int p, int b) {
   Color c;
+  // Normalize brightness
   double bright = convBrightness(b);
+  // Map pitch values to RGB components
+  int mappedRed = mapLinear(p, 239, 250, 0, 255);
+  int mappedGreen = mapLinear(p, 398, 653, 255, 0);
+  int mappedBlue = mapLinear(p, 398, 653, 0, 255);
 
-  if(p < 40) {
-    setColor(&c, 255, 0, 0);
-  }
-  else if(p >= 40 && p <= 77) {
-    int b = (p - 40) * (255/37.0000);
-    setColor(&c, 255, 0, b);
-  }
-  else if(p > 77 && p <= 205) {
-    int r = 255 - ((p - 78) * 2);
-    setColor(&c, r, 0, 255);
-  }
-  else if(p >= 206 && p <= 238) {
-    int g = (p - 206) * (255/32.0000);
-    setColor(&c, 0, g, 255);
-  }
-  else if(p <= 239 && p <= 250) {
-    int r = (p - 239) * (255/11.0000);
-    setColor(&c, r, 255, 255);
-  }
-  else if(p >= 251 && p <= 270) {
-    setColor(&c, 255, 255, 255);
-  }
-  else if(p >= 271 && p <= 398) {
-    int rb = 255-((p-271)*2);
-    setColor(&c, rb, 255, rb);
-  }
-  else if(p >= 398 && p <= 653) {
-    setColor(&c, 0, 255-(p-398), (p-398));
-  }
-  else {
-    setColor(&c, 255, 0, 0);
-  }
+  // Set color based on pitch range
+  if (p < 40) setColor(&c, 255, 0, 0);
+  else if (p <= 77) setColor(&c, 255, 0, mapLinear(p, 40, 77, 0, 255));
+  else if (p <= 205) setColor(&c, mappedRed, 0, 255);
+  else if (p <= 238) setColor(&c, 0, mapLinear(p, 206, 238, 0, 255), 255);
+  else if (p <= 250) setColor(&c, mappedRed, 255, 255);
+  else if (p <= 270) setColor(&c, 255, 255, 255);
+  else if (p <= 398) setColor(&c, mapLinear(p, 271, 398, 255, 0), 255, mappedBlue);
+  else setColor(&c, 255, 0, 0);
+
+  // Apply brightness to the color
   setColor(&c, c.r * bright, c.g * bright, c.b * bright);
   return c;
 }
 
-void setColor(Color *c, int r, int g, int b) {
+// Set RGB values for a Color structure
+inline void setColor(Color *c, uint8_t r, uint8_t g, uint8_t b) {
   c->r = r;
   c->g = g;
   c->b = b;
 }
 
-// Prints color structure data
-void printColor(Color c) {
-  Serial.print("( ");
-  Serial.print(c.r);
-  Serial.print(", ");
-  Serial.print(c.g);
-  Serial.print(", ");
-  Serial.print(c.b);
-  Serial.println(" )");
+// Shift the color spectrum
+void shiftColorSpectrum() {
+  shiftC += 200;
+  // Reset multiplier when it reaches a certain value
+  mulC = (shiftC >= 600) ? 2 : mulC + 1;
 }
 
+// Fill the LED array with black color
+void fillLedsWithBlack() {
+  std::fill(leds, leds + NUM_LEDS, CRGB(0, 0, 0));
+}
 
+// Rotate LED array to simulate motion
+void updateLeds() {
+  std::rotate(leds, leds + (NUM_LEDS - UPDATE_LEDS), leds + NUM_LEDS);
+}
+
+// Set color for the updated LEDs
+void setUpdatedLedsColor(Color nc) {
+  std::fill(leds, leds + UPDATE_LEDS, CRGB(nc.r, nc.g, nc.b));
+}
+
+// Linear mapping function
+inline int mapLinear(int value, int inMin, int inMax, int outMin, int outMax) {
+  return static_cast<int>((value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin);
+}
